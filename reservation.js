6d379aaf4d8d3aa78729ea4cf05e30a2eedb40ac
Jheudes <42938904+Jheudes@users.noreply.github.com>
2018-12-20 09:23:53 +0100

    Add files via upload

--- reservation.js
+++ reservation.js
@@ -1,52 +1,84 @@
 // JavaScript source code
 $(function () {
     var deadline;
-    function DeadLineSetup() {
-        var temporary;
-        temporary = sessionStorage.getItem('deadline', deadline);
-        $('#oki').html(deadline);
-        if (Date.parse(temporary) - Date.parse(new Date()) >= 1) { deadline = temporary; }
-        else { deadline = new Date(new Date().getTime() + 20 * 60 * 1000);}
+    var name12 = 0;
+
+    // Pour de boot de la page
+    var oklm = new Date(sessionStorage.getItem('deadline')).getTime();//deadline en ms
+    var compareDate = new Date();// date actuelle en ms
+    var nom = localStorage.getItem('nom');
+    var prenom = localStorage.getItem('prenom');
+
+    $('#prenom').val(prenom);  //prenom
+    $('#nom').val(nom);//nom
+    if (oklm != undefined && oklm >= compareDate) {// si la deadline n'est pas indefinie et que deadline > date actuelle ==> lancer le décompte
+        var address = sessionStorage.getItem('stationaddress');
+        var number = sessionStorage.getItem('stationnumber');
+        $('#infoderesa').html(address + number);
+        initializeClock();
     }
+    else { sessionStorage.clear();}
+    
     function SessionStorageSetup() {
         var temp = $('#number').text();
         var temp1 = $('#address').text();
-        sessionStorage.setItem('stationnumber', temp);
         sessionStorage.setItem('stationaddress', temp1);
+        sessionStorage.setItem('stationnumber', temp);
+        $('#infoderesa').html("Votre V&#233;lo est station&#233; " + temp1 + " Station N&#176; " + temp);
     }
+
     function getTimeRemaining() {
         var t = Date.parse(deadline) - Date.parse(new Date());//deadline - date du present
         var seconds = Math.floor((t / 1000) % 60);// /1000 pour recup en secondes, %60 car l'on veut le reste des minutes
-        var minutes = Math.floor((t / 1000 / 60) % 60);// /60 car on veut des heures et encore %60 pour en recup le reste
+        var minutes = Math.floor((t / 1000 / 60) % 60);// /60 car on veut des heures et encore %60 pour en recup le reste en secondes
         return {'total': t, 'minutes': minutes, 'seconds': seconds};
     }
+
     function initializeClock() {
-        $('#infoderesa').html($('#address').text() + $('#number').text());//permet d'afficher le numero de station
-        SessionStorageSetup();// mise en memoire de l'adresse et du numero de la station
-        DeadLineSetup();//set/reset de la deadline
+        var deadlineTest = sessionStorage.getItem('deadline', deadline);
+        if (deadlineTest != undefined) {
+            deadline = deadlineTest;
+            var address = sessionStorage.getItem('stationaddress');
+            var number = sessionStorage.getItem('stationnumber');
+            $('#infoderesa').html("Votre V&#233;lo est station&#233; " + address + " Station N&#176; " + number);
+        } else {
+            deadline = new Date(new Date().getTime() + 20 * 60 * 1000);
+            SessionStorageSetup();// mise en memoire de l'adresse et du numero de la station
+        }
+        
         sessionStorage.setItem('deadline', deadline);// mise en memoire de la deadline
 
         var timeinterval = setInterval(function () {//setInterval(xxx,1000); pour call getTimeRemaining() 
             var t = getTimeRemaining();
             if (t.total >= 1) {//si deadline est inferieur a temps present et qu'un reset n'a pas été call alors affiche xxx
-                $('#clockdiv').html('La reservation expire dans ' + t.minutes + ' minutes et ' + t.seconds + ' secondes');
+                $('#clockdiv').html("La reservation au nom de " + localStorage.getItem('nom') + " " + localStorage.getItem('prenom') )
+                $('#clockdiv2').html('expire dans ' + t.minutes + ' minutes et ' + t.seconds + ' secondes');
             }
             //else if (exec != "ok") {clearInterval(timeinterval);}//sinon si le reset a été call il arete la fonction
             else {//sinon il laisse le temps s'ecouler jusque 0 et l'affiche
-                $('#clockdiv').html('La reservation est expiree');
+                $('#clockdiv').html('La reservation est expirée');
+                ('#clockdiv2').html();
+                $('#infoderesa').html('Vous n\'avez plus de reservation en cours');
                 clearInterval(timeinterval);
             }
         }, 1000);
     }
-    
-    $('#clickMe').click(initializeClock);// lance la fonction au moment du click sur valider
+
+    $('#clickMe').click(function () {
+        sessionStorage.clear();
+        initializeClock();
+    });// lance la fonction au moment du click sur valider
     $('#clickMe2').click(function oki() { $('#oki').html(sessionStorage.getItem('deadline')); })
     $('#clickMe3').click(function oki() { sessionStorage.removeItem('deadline'); })
-    $('#clickMe4').click(function oki() { oki111(); })
-    function oki111() {
-        var temp = document.getElementById('prenom');
-        temp.addEventListener('input', function () { $('#oki4').html(temp.value);})
-    }
-
+    $('#clickMe4').click(function oki() { localStorage.removeItem('nom'); localStorage.removeItem('prenom'); })
+    $('#submit').click(function (event) {
+        event.preventDefault();
+        var nom = $('#nom').val()
+        var prenom = $('#prenom').val()
+        localStorage.setItem('nom', nom);
+        localStorage.setItem('prenom', prenom);
+        sessionStorage.clear();
+        initializeClock();
+    })
      
 });
---
+++ slider.js
@@ -0,0 +1,56 @@
+// JavaScript source code
+$(function () {
+    var slides = document.querySelectorAll('#slides .slide');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 2000);
+
+    function nextSlide() {
+        slides[currentSlide].className = 'slide';
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].className = 'slide showing';
+    }
+    function previousSlide() {
+        slides[currentSlide].className = 'slide';
+        currentSlide = (currentSlide + (slides.length - 1)) % slides.length;
+        slides[currentSlide].className = 'slide showing';
+    }
+
+    var playing = true;
+    var pauseButton = document.getElementById('pause');
+
+    function pauseSlideshow() {
+        pauseButton.innerHTML = 'Play';
+        playing = false;
+        clearInterval(slideInterval);
+    }
+
+    function playSlideshow() {
+        pauseButton.innerHTML = 'Pause';
+        playing = true;
+        slideInterval = setInterval(nextSlide, 1000);
+    }
+
+    pauseButton.onclick = function () {
+        if (playing) { pauseSlideshow(); }
+        else { playSlideshow(); }
+    };
+    $('#nextSlide').click(nextSlide);
+    $('#prevSlide').click(previousSlide);
+
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+
+        e = e || window.event;
+
+        if (e.keyCode == '37') {
+            previousSlide();//left
+        }
+        else if (e.keyCode == '39') {
+            nextSlide();//right
+        }
+    }
+    
+});
+
+
